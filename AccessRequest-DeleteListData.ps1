1d7271784596da2b9845cfe04726a9dd3110b19b
Pat Heard <patrick.heard@canada.ca>
2020-09-08 17:09:09 -0400

    Add script to delete all AccessRequest items

---
+++ AccessRequest-DeleteListData.ps1
@@ -0,0 +1,21 @@
+#Config Variables
+
+Param(
+    [Parameter(Mandatory=$True, HelpMessage="URL of the SharePoint site that contains the list: https://someurl.ca")]
+    [string]$SiteURL
+)
+
+$ListName = "AccessRequest"
+
+# Connect to site
+Connect-PnPOnline -Url $SiteUrl -UseWebLogin
+
+# Get 1000 items and delete
+$items = Get-PnPListItem -List $ListName -PageSize 1000
+
+Write-Host "Deleting $($items.count) items from $ListName"
+foreach ($item in $items)
+{
+    Write-Host "Removing $($item.Id)"
+    Remove-PnPListItem -List $ListName -Identity $item.Id -Force
+}
