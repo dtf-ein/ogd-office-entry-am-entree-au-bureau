d40ee4fcdf381baf15c6b5c292fdca4ae545ee7d
Pat Heard <patrick.heard@canada.ca>
2020-09-08 17:08:45 -0400

    Randomize start/end times and start date

--- AccessRequest-ApplyListData.ps1
+++ AccessRequest-ApplyListData.ps1
@@ -13,25 +13,32 @@ $ListName = "AccessRequest"
 ###To Do: investigate using for loop to iterate through columns
 
 #Get the CSV file contents
-$CSVData = Import-Csv -Path ($CSVFolder + $ListName + ".csv") -header "Title","StartHour","EndHour","FloorID","Status","VisitorCount","EntryDate","EntryDateID","EmployeeName","EmployeeEmail","ManagerLogin","ManagerName","ManagerEmail","EquipmentEnglishText","EquipmentFrenchText","ReasonEnglishText","ReasonFrenchText","ReasonDetail","BuildingID" -delimiter "|" -Encoding Unicode
+$CSVData = Import-Csv -Path ($CSVFolder + $ListName + ".csv") -header "Title","StartHour","EndHour","FloorID","Status","VisitorCount","EntryDate","EntryDateID","EmployeeLogin","EmployeeName","EmployeeEmail","ManagerLogin","ManagerName","ManagerEmail","EquipmentEnglishText","EquipmentFrenchText","ReasonEnglishText","ReasonFrenchText","ReasonDetail","BuildingID" -delimiter "|" -Encoding Unicode
 
 #Connect to site
 Connect-PnPOnline -Url $SiteUrl -UseWebLogin
 
 #Iterate through each Row in the CSV and import data to SharePoint Online List
+$count = 0
 foreach ($Row in $CSVData)
 {
 	if ($Row.Title -ne "Title")
 	{
 
+        # Randomize the request details
+        $StartHour = Get-Random -Minimum 9 -Maximum 12
+        $EndHour = Get-Random -Minimum 13 -Maximum 17
+        $EntryDate = (Get-Date).AddDays(++$count)
+
 		Add-PnPListItem -List $ListName -Values @{"Title" = $([guid]::NewGuid().toString())
-                            "StartHour" = $($Row.StartHour)
-                            "EndHour" = $($Row.EndHour)
+                            "StartHour" = $StartHour
+                            "EndHour" = $EndHour
+                            "EntryDate" = $EntryDate.toString("yyyy-MM-dd hh:mm:ss tt")
+                            "EntryDateID" = $EntryDate.toString("yyyyMMdd")
                             "FloorID" = $($Row.FloorID)
                             "Status" = $($Row.Status)
                             "VisitorCount" = $($Row.VisitorCount)
-                            "EntryDate" = $($Row.EntryDate)
-                            "EntryDateID" = $($Row.EntryDateID)
+                            "EmployeeLogin" = $($Row.EmployeeLogin)
                             "EmployeeName" = $($Row.EmployeeName)
                             "EmployeeEmail" = $($Row.EmployeeEmail)
                             "ManagerLogin" = $($Row.ManagerLogin)
